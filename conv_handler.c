279c07e99cbc6e391f04818fee7c9a0ef2a65af1
bethelhem920 <nbethelhem920@gmail.com>
2022-11-25 03:24:48 -0800

    betty handeled

--- conv_handler.c
+++ conv_handler.c
@@ -11,42 +11,52 @@
 int charPrint(int char_index, va_list list);
 int StringrPrint(int char_index, va_list list);
 
-<<<<<<< HEAD
-int conv_handler(char c, int char_index, va_list list){
-  int size = 0;
-  if(c == 'c'){
-    charPrint(char_index, list);
-    size = 1;
-  }
-  else if(c == 's'){
-    size = StringrPrint(char_index, list);
-  }
-  return (size);
-=======
+/*<<<<<<< HEAD*/
+int conv_handler(char c, int char_index, va_list list)
+{
+	int size = 0;
+
+	if (c == 'c')
+	{
+		charPrint(char_index, list);
+		size = 1;
+	}
+	else if (c == 's')
+	{
+		size = StringrPrint(char_index, list);
+	}
+	return (size);
+/*=======*/
+
 int conv_handler(char c, int char_index, va_list list)
 {
 int size = 0;
+
  /* int i; */
- if (c == 'c')
+if (c == 'c')
 {
 size = charPrint(char_index, list);
->>>>>>> cb49af7ca641cec8217dbd00be28042acd5a2887
+/*>>>>>>> cb49af7ca641cec8217dbd00be28042acd5a2887*/
 }
- else if (c == 's'){
-   size = StringrPrint(char_index, list);
- }
- else {
-   /** char *str  = va_arg(list, char *);
-   int sizeStr = 0; 
-   printf("%s", str);
-   _putchar('%');
-   for(i = 0; str && str[i] != '\0'; i++){
-     _putchar(str[i]);
-     size = i;
-   } */
-   return (-1);
- }
- return (size);
+else if (c == 's')
+{
+size = StringrPrint(char_index, list);
+}
+else
+{
+/**
+ * char *str  = va_arg(list, char *);
+ * int sizeStr = 0;
+ * printf("%s", str);
+ * _putchar('%');
+ * for(i = 0; str && str[i] != '\0'; i++){
+ * _putchar(str[i]);
+ * size = i;
+ * }
+ */
+return (-1);
+}
+return (size);
 }
 int charPrint(int char_index, va_list list)
 {
@@ -54,15 +64,15 @@ int i;
 if (char_index < 0)
 return (-1);
 
-/**va_start(list, format); */
+/*va_start(list, format); */
 for (i = 0; i <= char_index + 1; i++)
 {
-if (i == char_index)
-{
-    _putchar(va_arg(list, int));
+	if (i == char_index)
+	{
+		_putchar(va_arg(list, int));
+	}
 }
-}
-/**va_end(list); */
+/*va_end(list); */
 return (1);
 }
 
@@ -76,24 +86,25 @@ int j;
 if (char_index < 0)
 return (-1);
 
-/** va_start(list, format); */
+/* va_start(list, format); */
 for (i = 0; i <= char_index + 1; i++)
 {
 if (i == char_index)
 {
-  /** if(!va_arg(list, int)){
-    
-    char *nullStr = "(null)";
-    printf("is on null statment");
-    for(k = 0; k < 6; k++){
-      _putchar(nullStr[k]);
-    }
-    return (6);
-  } */
-     
+/**
+ * if(!va_arg(list, int)){
+ * char *nullStr = "(null)";
+ * printf("is on null statment");
+ * for(k = 0; k < 6; k++){
+ * _putchar(nullStr[k]);
+ * }
+ * return (6);
+ * }
+ */
+
 str = va_arg(list, char *);
- for (j = 0; str && str[j] != '\0'; j++)
-{  
+for (j = 0; str && str[j] != '\0'; j++)
+{ 
 size += 1;
 _putchar(str[j]);
 }
