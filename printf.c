2e2d265f0644867a4feba3da23f21768f816db17
kidist1234 <kidistmamaye2002@gmail.com>
2022-11-24 23:32:43 -0800

    error handling

--- printf.c
+++ printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 int _printf(const char *format, ...){
-  int size=0;
+  unsigned size=0;
   va_list list;
   int i;
   int conCounter = 0;
@@ -12,7 +12,7 @@ int _printf(const char *format, ...){
   for(i = 0; format && format[i] != '\0'; i++){
     if (format[i] == '%'){
       if(format[i+1] == '\0'){
-        size = 0;
+        size = -1;
         return (size);
       }
       if(format[i+1] != '%'){
